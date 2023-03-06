226f6e2c322db6d2bb683cfbad9def2c67781d10
Fabian Johansson <fabjo285@student.liu.se>
2023-03-06 11:31:58 +0100

    Fixed formatting

--- libs/src/hello.cpp
+++ libs/src/hello.cpp
@@ -6,10 +6,8 @@
 auto main() -> int {
   std::cout << "test" << std::endl;
 
-        auto *test = new HelloClass(3, "test");
-
-  test->increase_num(5)
-      ;
+  auto *test = new HelloClass(3, "test");
+  test->increase_num(5);
 
   return 0;
 }
