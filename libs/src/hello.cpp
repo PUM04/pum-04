3d0204edd06aabfa3deb8a433777fc369e495a6c
Fabian Johansson <fabjo285@student.liu.se>
2023-03-06 09:27:08 +0100

    Added cpp files

---
+++ libs/src/hello.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+
+#include "hello_class.h"
+
+auto main() -> int {
+  std::cout << "test" << std::endl;
+
+  auto *test = new HelloClass(3, "test");
+
+  test->increase_num(5);
+
+  return 0;
+}
