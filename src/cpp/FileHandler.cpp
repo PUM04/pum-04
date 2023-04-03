730ba01f4e5bc8f123e2d91cb861b143d9efafd6
Albin Wilander <albin.wilander@gmail.com>
2023-04-03 11:50:05 +0200

    Added regex for getting metric info

--- src/cpp/FileHandler.cpp
+++ src/cpp/FileHandler.cpp
@@ -4,4 +4,9 @@
 
 void FileHandler::add_file(std::string file) {
     std::cout << file;
+    /*
+    Get metric/method name (?<={(metric|method)=")\w*(?=",le="(\d*|\+Inf)"} \d*\n)
+    Get length (?<={(metric|method)="\w*",le=")(\d*|\+Inf)(?=")
+    Get count for both metrics and methods: (?<={(metric|method)="\w*",le="(\d*|\+Inf)"} )\d*\n
+    */
 }
