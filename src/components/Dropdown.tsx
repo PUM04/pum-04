f300e6579dedd9d702e71e29f20c765e3e2fefe0
alges694 <alges694@student.liu.se>
2023-04-05 14:06:26 +0200

    changed styling on dropdown

--- src/components/Dropdown.tsx
+++ src/components/Dropdown.tsx
@@ -85,7 +85,7 @@ export default function Dropdown(props: dropdownProps): JSX.Element {
         justifyContent: 'center',
         '& > :not(style)': {
           m: 1,
-          width: 128,
+          width: 1,
           height: 1,
         },
       }}
@@ -101,7 +101,7 @@ export default function Dropdown(props: dropdownProps): JSX.Element {
         <ListItemText primary={dropdownName} />
         {open ? <ExpandLess /> : <ExpandMore />}
       </ListItemButton>
-      <Collapse in={open} timeout="auto" unmountOnExit>
+      <Collapse in={open} timeout={10} unmountOnExit>
         <List component="div" disablePadding>
           {content.map(({ item, selected }) => (
             <ListItemButton>
