ddb7ca803a1176e603850c42267aff422151e694
anton <anton.ostman123@gmail.com>
2023-04-06 11:59:14 +0200

    Add useEffect to re-render when children changes.

--- src/components/SectraTheme.tsx
+++ src/components/SectraTheme.tsx
@@ -1,9 +1,11 @@
 /**
  * @file
  */
-import { createTheme, ThemeProvider } from '@mui/material/styles';
-import React from 'react';
 
+import React, { useEffect, useState } from 'react';
+
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+import { Theme } from '@mui/material/styles/createTheme';
 // Extend the theme type to include our custom color
 
 declare module '@mui/material/styles' {
@@ -30,49 +32,57 @@ declare module '@mui/material/styles' {
 
 /**
  * Create a theme from the .mui-theme element in the DOM
+ * Re-renders the component when children changes
  *
  * @param props - props to pass to the ThemeProvider component that is returned from this function call (children)
  * @returns ThemeProvider component with a theme created from the .mui-theme element in the DOM
  */
 export default function CreateSectraTheme(props: any): JSX.Element {
-  const { children } = props;
-  const muiTheme = document.querySelector('.mui-theme');
-  console.log('tja');
-  if (!muiTheme) {
-    const theme = createTheme({
-      palette: {
-        primary: {
-          light: '#DEE8FA',
-          main: '#AECDEB',
-          dark: '#004688',
-        },
-        secondary: {
-          main: '#FF7B30',
-        },
-      },
-    });
-    return <ThemeProvider theme={theme}>{children}</ThemeProvider>;
-  }
-
-  // Get styles from .mui-theme and create a theme from them to pass to the ThemeProvider
-  const style = getComputedStyle(muiTheme);
-
-  const theme = createTheme({
+  const defaultTheme = createTheme({
     palette: {
       primary: {
-        light: style.getPropertyValue('--primary-light'),
-        main: style.getPropertyValue('--primary-main'),
-        dark: style.getPropertyValue('--primary-dark'),
-        light1: style.getPropertyValue('--primary-light1'),
-        light2: style.getPropertyValue('--primary-light2'),
-        light3: style.getPropertyValue('--primary-light3'),
-        light4: style.getPropertyValue('--primary-light4'),
+        light: '#DEE8FA',
+        main: '#AECDEB',
+        dark: '#004688',
       },
       secondary: {
-        main: style.getPropertyValue('--secondary-main'),
+        main: '#FF7B30',
       },
-      gray: { main: style.getPropertyValue('--gray-main') },
     },
   });
+
+  const [theme, setTheme] = useState<Theme>(defaultTheme);
+  const { children } = props;
+
+  useEffect(() => {
+    const muiTheme = document.querySelector('.mui-theme');
+    console.log('tja');
+    if (!muiTheme) {
+      setTheme(defaultTheme);
+    } else {
+      // Get styles from .mui-theme and create a theme from them to pass to the ThemeProvider
+      const style = getComputedStyle(muiTheme);
+      setTheme(
+        createTheme({
+          palette: {
+            primary: {
+              light: style.getPropertyValue('--primary-light'),
+              main: style.getPropertyValue('--primary-main'),
+              dark: style.getPropertyValue('--primary-dark'),
+              light1: style.getPropertyValue('--primary-light1'),
+              light2: style.getPropertyValue('--primary-light2'),
+              light3: style.getPropertyValue('--primary-light3'),
+              light4: style.getPropertyValue('--primary-light4'),
+            },
+            secondary: {
+              main: style.getPropertyValue('--secondary-main'),
+            },
+            gray: { main: style.getPropertyValue('--gray-main') },
+          },
+        })
+      );
+    }
+  }, [children]);
+
   return <ThemeProvider theme={theme}>{children}</ThemeProvider>;
 }
