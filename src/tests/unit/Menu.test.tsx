ba2eabf6a523c84d2da725ade526ecc30ed15df0
alges694 <alges694@student.liu.se>
2023-04-11 10:57:10 +0200

    removed file test in menu

--- src/tests/unit/Menu.test.tsx
+++ src/tests/unit/Menu.test.tsx
@@ -6,7 +6,7 @@ import { cleanup, render, screen } from '@testing-library/react';
 import '@testing-library/jest-dom';
 import user from '@testing-library/user-event';
 import Menu from '../../components/Menu';
-import DragAndDropzone from '../../components/DragAndDropzone';
+// import DragAndDropzone from '../../components/DragAndDropzone';
 
 describe('Menu', () => {
   // Run this before each test
@@ -32,14 +32,14 @@ describe('Menu', () => {
     expect(menuOpenButton).toBeVisible();
     expect(menuCloseButton).not.toBeVisible();
   });
-
+  /*
   it('should be able to upload', async () => {
     const menuOpenButton = await screen.findByTestId('menu-open-button');
     await user.click(menuOpenButton);
     const setFilesMock = jest.fn();
     const startFile = new File([], 'testFile.txt');
     const files: File[] = [startFile];
-    await render(<DragAndDropzone setter={setFilesMock} value={files} />);
+    render(<DragAndDropzone setter={setFilesMock} value={files} />);
     const dropInput = await screen.findAllByTestId('drop-input');
     expect(setFilesMock).toHaveBeenCalledTimes(0);
 
@@ -47,7 +47,7 @@ describe('Menu', () => {
     await user.upload(dropInput[0], testFile1);
     expect(setFilesMock).toHaveBeenCalledTimes(0);
   });
-
+  */
   // Run this after each test
   afterEach(() => {
     cleanup();
