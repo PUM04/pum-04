6dc53c7391ec723dce8c199d0316cd79f3df99c2
Cajsa Wargren <cajsa.wargren@d-sektionen.se>
2023-04-12 11:28:55 +0200

    linting, CLRF -> LF

--- src/tests/unit/baseComponent.test.tsx
+++ src/tests/unit/baseComponent.test.tsx
@@ -1,30 +1,31 @@
+/**
+ * @file Contains tests for the basecomponent
+ */
 import React from 'react';
-import { cleanup, render, screen} from '@testing-library/react';
-import user from '@testing-library/user-event';
+import { cleanup, render, screen } from '@testing-library/react';
 
-import { InfoboxComponent, GraphComponent} from '../../components/BaseComponent';
+import {
+  InfoboxComponent,
+  GraphComponent,
+} from '../../components/BaseComponent';
 
 describe('App', () => {
-    // Run this before each test
-    beforeEach(() => {
-        render(<GraphComponent />);
-        render(<InfoboxComponent />);
-    });
-  
-    // Tshould render 
-    it('should render graphComponent', async () => {
-      const graphBox = await screen.findByTestId('graph-component');
-    });
-  
-    // Tshould render 
-    it('should render graphComponent', async () => {
-        const infoBox = await screen.findByTestId('infobox-component');
-    });
+  // Run this before each test
+  beforeEach(() => {
+    render(<GraphComponent />);
+    render(<InfoboxComponent />);
+  });
 
-    // Run this after each test
-    afterEach(() => {
-      cleanup();
-    });
+  it('should render graphComponent', async () => {
+    await screen.findByTestId('graph-component');
+  });
+
+  it('should render graphComponent', async () => {
+    await screen.findByTestId('infobox-component');
+  });
+
+  // Run this after each test
+  afterEach(() => {
+    cleanup();
+  });
 });
-
-
