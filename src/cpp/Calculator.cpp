12a77d0319a294891b6e72822757d5d518791406
Fabian Johansson <fabjo285@student.liu.se>
2023-04-17 10:28:59 +0200

    Readded Calculator

---
+++ src/cpp/Calculator.cpp
@@ -0,0 +1,34 @@
+#include <emscripten/bind.h>
+
+// compile this with
+// emcc -lembind -o Calculator.js Calculator.cpp -s EXPORT_ES6=1 -s MODULARIZE=1
+
+int increment(int i)
+{
+    return ++i;
+}
+
+class Calculator
+{
+public:
+    Calculator()
+    {
+    }
+
+    int add(int x, int y)
+    {
+        return x + y;
+    }
+
+    int subtract(int x, int y)
+    {
+        return x - y;
+    }
+};
+
+using namespace emscripten;
+EMSCRIPTEN_BINDINGS(calculator)
+{
+    function("increment", &increment);
+    class_<Calculator>("Calculator").constructor().function("add", &Calculator::add).function("subtract", &Calculator::subtract);
+}
