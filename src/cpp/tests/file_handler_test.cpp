3fac45eb60883e145186ca22526f0ae19e219dc4
Rasmus Holmgren <ras.holmgren@gmail.com>
2023-04-18 10:31:47 +0200

    Add tests for GetMetric

--- src/cpp/tests/file_handler_test.cpp
+++ src/cpp/tests/file_handler_test.cpp
@@ -98,6 +98,105 @@ TEST_CASE("FileHandler") {
         CHECK(site_names2["names"][0] == "test2");
     }
 
-    SUBCASE("Get metrics") {}
+    SUBCASE("Get metrics with one file") {
+        std::string performance =
+            "response_time_bucket{method=\"Test\",le=\"1\"} 1\n"
+            "response_time_bucket{method=\"Test\",le=\"2\"} 1\n"
+            "response_time_bucket{method=\"Test\",le=\"3\"} 3\n"
+            "response_time_bucket{method=\"Test\",le=\"4\"} 6\n"
+            "response_time_bucket{method=\"Test\",le=\"5\"} 6\n"
+            "response_time_bucket{method=\"Test\",le=\"6\"} 8\n"
+            "response_time_bucket{method=\"Test1\",le=\"6\"} 1\n"
+            "response_time_bucket{method=\"Test2\",le=\"6\"} 1\n";
+
+        std::string performance_name = "test123_230102.txt";
+
+        std::string host =
+            "{\"site_name\": \"test\", \"site_id\": \"test123\"}";
+        std::string host_name = "test.json";
+
+        fh->AddFile(performance, performance_name);
+        fh->AddFile(host, host_name);
+        fh->ComputeFiles();
+        json metrics = json::parse(fh->GetMetrics());
+
+        CHECK(metrics["metrics"].size() == 3);
+        CHECK(metrics["metrics"][0] == "Test");
+        CHECK(metrics["metrics"][1] == "Test1");
+        CHECK(metrics["metrics"][2] == "Test2");
+    }
+
+    SUBCASE("Get metrics with multiple performance files") {
+        std::string performance1 =
+            "response_time_bucket{method=\"Test\",le=\"1\"} 1\n"
+            "response_time_bucket{method=\"Test1\",le=\"6\"} 1\n"
+            "response_time_bucket{method=\"Test2\",le=\"6\"} 1\n";
+
+        std::string performance1_name = "test1_230102.txt";
+
+        std::string host = "{\"site_name\": \"test\", \"site_id\": \"test1\"}";
+        std::string host_name = "test.json";
+
+        fh->AddFile(performance1, performance1_name);
+        fh->AddFile(host, host_name);
+
+        std::string performance =
+            "response_time_bucket{method=\"Test\",le=\"1\"} 1\n"
+            "response_time_bucket{method=\"Test1\",le=\"6\"} 1\n"
+            "response_time_bucket{method=\"Test3\",le=\"6\"} 1\n";
+
+        std::string performance2_name = "test1_230103.txt";
+        fh->AddFile(performance, performance2_name);
+
+        fh->ComputeFiles();
+        json metrics = json::parse(fh->GetMetrics());
+
+        CHECK(metrics["metrics"].size() == 4);
+        CHECK(metrics["metrics"][0] == "Test");
+        CHECK(metrics["metrics"][1] == "Test1");
+        CHECK(metrics["metrics"][2] == "Test2");
+        CHECK(metrics["metrics"][3] == "Test3");
+    }
+
+    SUBCASE("Get metrics with multiple host files") {
+        std::string performance1 =
+            "response_time_bucket{method=\"Test\",le=\"1\"} 1\n"
+            "response_time_bucket{method=\"Test1\",le=\"6\"} 1\n"
+            "response_time_bucket{method=\"Test2\",le=\"6\"} 1\n";
+
+        std::string performance1_name = "testid1_230102.txt";
+
+        std::string host1 =
+            "{\"site_name\": \"test1\", \"site_id\": \"testid1\"}";
+        std::string host1_name = "test.json";
+
+        fh->AddFile(performance1, performance1_name);
+        fh->AddFile(host1, host1_name);
+
+        std::string performance2 =
+            "response_time_bucket{method=\"Test\",le=\"1\"} 1\n"
+            "response_time_bucket{method=\"Test1\",le=\"6\"} 1\n"
+            "response_time_bucket{method=\"Test3\",le=\"6\"} 1\n";
+
+        std::string performance2_name = "testid2_230103.txt";
+
+        std::string host2 =
+            "{\"site_name\": \"test2\", \"site_id\": \"testid2\"}";
+        std::string host2_name = "test.json";
+
+        fh->AddFile(performance2, performance2_name);
+        fh->AddFile(host2, host2_name);
+
+        fh->ComputeFiles();
+        json metrics = json::parse(fh->GetMetrics());
+        std::cout << metrics.dump() << std::endl;
+
+        CHECK(metrics["metrics"].size() == 4);
+        CHECK(metrics["metrics"][0] == "Test");
+        CHECK(metrics["metrics"][1] == "Test1");
+        CHECK(metrics["metrics"][2] == "Test2");
+        CHECK(metrics["metrics"][3] == "Test3");
+    }
+
     delete fh;
 }
